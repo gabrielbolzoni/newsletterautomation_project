0d265124c0688935009dd16ac245fab15ed17115
Gabriel  Bolzoni <gabrielbolzoni@finor.tech>
2026-04-26 11:34:10 -0300

    fix: variable names correction

--- dags/dag.py
+++ dags/dag.py
@@ -9,7 +9,7 @@ from airflow.models import Variable
 
 # Na DAG ou na task
 audio_model_config = Variable.get("audio_model_config")
-audio__model_config_json = json.loads(audio_model_config)
+audio_model_config_json = json.loads(audio_model_config)
 bucket_name = Variable.get("s3_bucket_name")
 
 # ── DAG definition ──────────────────────────────────────────────────────────
@@ -42,7 +42,7 @@ with DAG(
         return cleaned_content
 
     @task()
-    def generate_audios(cleaned_content: list, audio__model_config_json: dict):
+    def generate_audios(cleaned_content: list, audio_model_config_json: dict):
         from utils.audio_generator import generate_audio
         with open("/opt/airflow/config/credentials/api_keys.json", "r") as f:
             credentials_file = json.load(f)
@@ -52,7 +52,7 @@ with DAG(
         os.makedirs(audio_folder_path, exist_ok=True)
         os.makedirs(text_folder_path, exist_ok=True)
 
-        paths = [generate_audio(content, credentials_file, audio__model_config_json) for content in cleaned_content]
+        paths = [generate_audio(content, credentials_file, audio_model_config_json) for content in cleaned_content]
         return paths    
     @task
     def send_audio(paths: list, bucket_name: str):
@@ -63,17 +63,12 @@ with DAG(
         for path in paths:
             object_name = os.path.basename(path)
             upload_file(credentials_file,path,bucket_name,object_name)
-    @task
-    def teste(audio_config_json):
-        print(audio_config_json)
-        print(type(audio_config_json))
         
         
-
     # ── Sequenciamento ───────────────────────────────────────────────────────
     news         = read_emails()
     extracted    = extract_content(news)
     cleaned      = filter_content(extracted)
-    audio_files  = generate_audios(cleaned,audio__model_config_json)
+    audio_files  = generate_audios(cleaned,audio_model_config_json)
     send_audio(audio_files,bucket_name)
     
