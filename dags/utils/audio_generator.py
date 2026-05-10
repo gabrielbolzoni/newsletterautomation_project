fb96c8453f71f3dfaf7835f1729bc33da59e542c
Gabriel  Bolzoni <gabrielbolzoni@finor.tech>
2026-05-10 20:59:34 -0300

    refactor: create two separated functions to handle different tts models

--- dags/utils/audio_generator.py
+++ dags/utils/audio_generator.py
@@ -1,7 +1,71 @@
 from pathlib import Path
 from datetime import date
-from pathlib import Path
 from openai import OpenAI
+from elevenlabs.client import ElevenLabs
+
+
+def openAI_model(newsletter_content: dict, credentials_file, audio_parameters, audio_file_path: Path):
+    instructions = """
+            Você vai narrar as notícias abaixo em áudio, como se estivesse conversando com um amigo próximo.
+
+            TOM:
+            - Conversa informal, natural e envolvente.
+            - Linguagem simples, como alguém contando algo interessante que acabou de ler.
+            - Evite tom de locutor, telejornal ou publicidade.
+            - Não soe robótico nem monótono.
+
+            ESTILO DE FALA:
+            - Varie levemente o ritmo e a entonação entre as notícias.
+            - Use pausas naturais entre os blocos, como em uma conversa.
+            - Demonstre curiosidade ou leve surpresa quando fizer sentido.
+            - Não seja exagerado, nem teatral.
+
+            REGRAS:
+            - Não use chamadas comerciais ou frases promocionais.
+            - Não adicione opiniões pessoais fortes.
+            - Não invente fatos.
+
+            FORMATO:
+            - Conte cada notícia como uma história curta.
+            - Separe as notícias com uma pausa perceptível, não verbal.
+            - Não anuncie início ou fim do áudio."""
+
+    client = OpenAI(api_key=credentials_file["openAI"])
+
+    speed = audio_parameters["speed"]
+    voice = audio_parameters["voice"]
+
+    with client.audio.speech.with_streaming_response.create(
+        model="gpt-4o-mini-tts",
+        instructions=instructions,
+        speed=speed,
+        voice=voice,
+        input=newsletter_content["content"]
+    ) as response:
+        response.stream_to_file(audio_file_path)
+
+
+def elevenLabs_model(newsletter_content: dict, credentials_file, audio_parameters, audio_file_path: Path):
+    client = ElevenLabs(api_key=credentials_file["elevenLabs"])
+
+    speed = audio_parameters["speed"]
+    voice = audio_parameters["voice"]
+    
+
+    audio_stream = client.text_to_speech.convert(
+        text=newsletter_content["content"],
+        voice_id=voice,
+        model_id="eleven_flash_v2_5",
+        voice_settings={
+            "stability": "0.5",
+            "speed": speed,
+        },
+        output_format="mp3_44100_128",
+    )
+
+    with open(audio_file_path, "wb") as f:
+        for chunk in audio_stream:
+            f.write(chunk)
 
 
 def generate_audio(newsletter_content: dict, credentials_file, audio_model_config_json: dict):
@@ -22,54 +86,20 @@ def generate_audio(newsletter_content: dict, credentials_file, audio_model_confi
             dictionary with the TTS model configuration (model, voice, speed)
 
     Returns:
-        file_path: path of the generated audio.It is passed to the function responsible for sending the audio to the S3 bucket.
+        str: path of the generated audio, passed to the S3 upload function.
     """
-    client = OpenAI(
-    api_key=credentials_file["openAI"],
-        )    
-    model = audio_model_config_json["model"]
-    speed = audio_model_config_json["speed"]
-    voice = audio_model_config_json["voice"]
 
     source = newsletter_content["source"]
     file_name = f"{source}_{date.today().strftime('%Y_%m_%d')}.mp3"
     audio_folder_path = Path("/opt/airflow/data/audio_files")
-    file_path = audio_folder_path.joinpath(file_name)
-    with client.audio.speech.with_streaming_response.create(
-        model = model ,
-        instructions = """
-                Você vai narrar as notícias abaixo em áudio, como se estivesse conversando com um amigo próximo.
-
-                TOM:
-                - Conversa informal, natural e envolvente.
-                - Linguagem simples, como alguém contando algo interessante que acabou de ler.
-                - Evite tom de locutor, telejornal ou publicidade.
-                - Não soe robótico nem monótono.
-
-                ESTILO DE FALA:
-                - Varie levemente o ritmo e a entonação entre as notícias.
-                - Use pausas naturais entre os blocos, como em uma conversa.
-                - Demonstre curiosidade ou leve surpresa quando fizer sentido.
-                - Não seja exagerado, nem teatral.
-
-                REGRAS:
-                - Não use chamadas comerciais ou frases promocionais.
-                - Não adicione opiniões pessoais fortes.
-                - Não invente fatos.
-
-                FORMATO:
-                - Conte cada notícia como uma história curta.
-                - Separe as notícias com uma pausa perceptível, não verbal.
-                - Não anuncie início ou fim do áudio.""",
-        speed = speed,
-        voice= voice,
-        input=f"""
-                News Letter: {source}
-                {newsletter_content["content"]}
-                """
-        ) as response:
-                response.stream_to_file(file_path)
-                
-    return str(file_path)
+    audio_file_path = audio_folder_path.joinpath(file_name)
 
 
+    if audio_model_config_json["model"] == "openAI":
+        openAI_model(newsletter_content, credentials_file, audio_model_config_json, audio_file_path)
+    elif audio_model_config_json["model"] == "elevenLabs":
+        elevenLabs_model(newsletter_content, credentials_file, audio_model_config_json, audio_file_path)
+    else:
+        raise ValueError(f"Unsupported model: {audio_model_config_json['model']}")
+
+    return str(audio_file_path)
